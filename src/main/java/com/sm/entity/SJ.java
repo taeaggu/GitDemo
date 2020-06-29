84981804cac8ba4f01a5f197f1ce0144e079f14b
wendy_glb <todayiswendy_glb@sm.com>
2020-06-29 14:38:03 +0800

    SJ entity commit

---
+++ src/main/java/com/sm/entity/SJ.java
@@ -0,0 +1,27 @@
+package com.sm.entity;
+
+public class SJ {
+
+	private String id;
+	
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
