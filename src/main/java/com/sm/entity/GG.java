20957132b632715e638109a8e4a124b414da7645
wendy_glb <todayiswendy_glb@sm.com>
2020-06-30 09:08:45 +0800

    hotfix commit

--- src/main/java/com/sm/entity/GG.java
+++ src/main/java/com/sm/entity/GG.java
@@ -2,4 +2,24 @@ package com.sm.entity;
 
 public class GG {
 
+	private String id;
+
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
